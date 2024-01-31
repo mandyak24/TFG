e34fb535fd44e0f25a0975f0ff59489b7358d374
Amanda <amandaroblesurena@gmail.com>
2024-01-31 11:31:15 +0100

    Primer commit con mi codigo

---
+++ Vista/vistaLogin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>M-M-BABY-D-Login 🍼</title>
+    <link rel="stylesheet" href="../css/login.css">
+</head>
+
+<body>
+    <section>
+        <div class="container">
+       
+            <div class="form">
+                <form action="#" method="POST">
+                <h2>MAKE MY BABY DAY</h2><br>
+                    <div class="input-container">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" id="email" placeholder="Email" required>
+                    </div>
+                    <div class="input-container">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" id="password" placeholder="Password" required>
+                    </div>
+                    <div class="input-container">
+                        <input type="checkbox">
+                        <a href="#">Remember me</a>
+                    </div>
+                </form>
+
+                <div>
+                    <button type="submit" name="login">Login</button>
+                    <div class="register">
+                        <p>Don't have an account? <a href="#">Sign up</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
